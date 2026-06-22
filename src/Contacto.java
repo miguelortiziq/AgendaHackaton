09eb11ee042a2ee584fd91514e922418e8d5f314
MiguelOrtiz <miguel.ort.iq@gmail.com>
2026-06-22 12:20:25 -0600

    Clase Contacto

---
+++ src/Contacto.java
@@ -0,0 +1,23 @@
+public class Contacto {
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+//Nombres y apellidos no deben estar vacios
+
+/*
+Un contacto se considera igual a otro cuando tienen el mismo nombre y
+apellido (sin distinguir entre mayúsculas y minúsculas)
+*/
+
+    public Contacto(String nombre, String apellido, String telefono) { // Constructor que recibe nombre y teléfono.
+        this.nombre = nombre;
+        this.apellido=apellido;// Asignamos el nombre recibido al atributo nombre.
+        this.telefono = telefono; // Asignamos el teléfono recibido al atributo telefono.
+
+    }
+
+
+
+}
+
